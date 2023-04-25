70b55176d5ed76f3fc118973267f02a4e089e367
Lucas N <98057712+Luccxx@users.noreply.github.com>
2023-04-25 17:15:12 -0300

    Add files via upload

---
+++ ex2/index.js
@@ -0,0 +1,23 @@
+function carregar(){
+    //var data = new Date();
+    var label_hora = window.document.getElementById('label');
+    var body = document.body;
+    var titulo = document.getElementById('titulo')
+    var img = document.getElementById('img');
+    var hora_atual = 17;
+    label_hora.innerHTML = `A hora atual é: ${hora_atual}:00`;
+    if (hora_atual >0 && hora_atual <12) {
+        titulo.innerHTML = "Bom Dia!"
+        body.style.backgroundColor = "skyblue";
+        img.src = 'img/manha.jpg';
+    } else if (hora_atual >=12 && hora_atual<18) {
+        titulo.innerHTML = "Boa Tarde!"
+        body.style.backgroundColor = "green";
+        img.src = "img/tarde.jpg"
+    } else{
+        titulo.innerHTML = "Boa noite!"
+        body.style.backgroundColor = "darkblue";
+        body.style.color = "white"
+        img.src = "img/noite.jpg"
+    }
+}
